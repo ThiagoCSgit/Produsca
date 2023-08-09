0810a940dc753bb1f0ecd0d2a8443c0f25100636
ThiagoCSgit <thiago.csdossantos@gmail.com>
2023-08-09 01:09:22 -0300

    identação

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -100,97 +100,97 @@ export default function Products({ route, navigation }) {
 
   return ( isLoadingHistory && isLoadingMarkets ?
     <Loading/> :
-      <ScrollView>
-    <SafeAreaView style={styles.container}>
-        <Text style={styles.nameProduct}>{nameProduct}</Text>
-        <View>
-          {
-            priceHistory.length > 0 &&
-            <LineChart
-              data={{
-                labels: priceHistory.map(item => {
-                  return format(new Date(`${item.data}`), 'dd MMMM yy', { locale: ptBR });
-                }),
-                datasets: [{
-                  data: priceHistory.map(item => item.preco)
-                }]
-              }}
-              width={Dimensions.get("window").width - 20} // from react-native
-              height={320}
-              yAxisLabel="R$"
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundGradientFrom: "#5682e8",
-                backgroundGradientTo: "#005fff",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                propsForLabels: {
-                  fontSize: 15, // Defina o tamanho da fonte desejado aqui
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#00f1ff"
-                }
-              }}
-              bezier
-              style={{
-                marginTop: 20,
-                borderRadius: 5,
-              }}
-            />
-          }
-          <View style={styles.selectDays}>
-            {
-              <RadioButtonDays quantDays={quantDays} setQuantDays={setQuantDays}/>
-            }
-          </View>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+          <Text style={styles.nameProduct}>{nameProduct}</Text>
           <View>
-            <View style={styles.listSupermarktesAvailables}>
-              <Text style={{fontSize: 20, marginBottom: 10, fontFamily: "OpenSans_500Medium"}}>Disponível em:</Text>
-              {supermarktesAvailables.map(item => (
-                <View style={styles.itemSupermarket}>
-                  <View style={{flexDirection: "row", justifyContent: "center", paddingBottom: 10}}>
-                    <TouchableOpacity onPress={() => navigation.navigate("Supermercado", {
-                      name: item.nome
-                    })}>
-                      <Text style={{color: "#1E90FF", fontSize: 20, fontFamily: "OpenSans_500Medium"}}>
-                        Supermercado {item.nome}
-                      </Text>
+            {
+              priceHistory.length > 0 &&
+              <LineChart
+                data={{
+                  labels: priceHistory.map(item => {
+                    return format(new Date(`${item.data}`), 'dd MMMM yy', { locale: ptBR });
+                  }),
+                  datasets: [{
+                    data: priceHistory.map(item => item.preco)
+                  }]
+                }}
+                width={Dimensions.get("window").width - 20} // from react-native
+                height={320}
+                yAxisLabel="R$"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundGradientFrom: "#5682e8",
+                  backgroundGradientTo: "#005fff",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  propsForLabels: {
+                    fontSize: 15, // Defina o tamanho da fonte desejado aqui
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#00f1ff"
+                  }
+                }}
+                bezier
+                style={{
+                  marginTop: 20,
+                  borderRadius: 5,
+                }}
+              />
+            }
+            <View style={styles.selectDays}>
+              {
+                <RadioButtonDays quantDays={quantDays} setQuantDays={setQuantDays}/>
+              }
+            </View>
+            <View>
+              <View style={styles.listSupermarktesAvailables}>
+                <Text style={{fontSize: 20, marginBottom: 10, fontFamily: "OpenSans_500Medium"}}>Disponível em:</Text>
+                {supermarktesAvailables.map(item => (
+                  <View style={styles.itemSupermarket}>
+                    <View style={{flexDirection: "row", justifyContent: "center", paddingBottom: 10}}>
+                      <TouchableOpacity onPress={() => navigation.navigate("Supermercado", {
+                        name: item.nome
+                      })}>
+                        <Text style={{color: "#1E90FF", fontSize: 20, fontFamily: "OpenSans_500Medium"}}>
+                          Supermercado {item.nome}
+                        </Text>
+                      </TouchableOpacity>
+                      <Text style={{fontSize: 20, color: "#623b32", fontFamily: "OpenSans_500Medium"}}> - R${item.produto.preco}</Text>
+                    </View>
+                    <TouchableOpacity style={styles.buttonHistoric} onPress={() => 
+                      navigation.navigate("Detalhes do Produto", {
+                        supermarket: item.nome,
+                        nameProduct: nameProduct,
+                        idProduct: idProduct,
+                        funcAddRemoveCart: funcAddRemoveCart
+                      })
+                    }>
+                      <Text style={styles.buttonHistoricText}>Histórico de preço</Text>
                     </TouchableOpacity>
-                    <Text style={{fontSize: 20, color: "#623b32", fontFamily: "OpenSans_500Medium"}}> - R${item.produto.preco}</Text>
                   </View>
-                  <TouchableOpacity style={styles.buttonHistoric} onPress={() => 
-                    navigation.navigate("Detalhes do Produto", {
-                      supermarket: item.nome,
-                      nameProduct: nameProduct,
-                      idProduct: idProduct,
-                      funcAddRemoveCart: funcAddRemoveCart
-                    })
-                  }>
-                    <Text style={styles.buttonHistoricText}>Histórico de preço</Text>
-                  </TouchableOpacity>
+                ))}
+                <View style={styles.buttonsArea}>
+                  <Pressable onPress={() => executeAction(inCart, idProduct)}>
+                    <View style={{flexDirection: "row", marginLeft: 10}}>
+                      <Checkbox
+                        value={inCart}
+                        style={{height: 25, width: 25}}
+                      />
+                      <Text style={styles.labelCheckBox}>Adicionar ao carrinho</Text>
+                    </View>
+                  </Pressable>
+                  <Pressable onPress={onShare}>
+                    <IconFE style={{marginRight: 25, height: 30, width: 30}} name="share-2" size={27} />
+                  </Pressable>
                 </View>
-              ))}
-              <View style={styles.buttonsArea}>
-                <Pressable onPress={() => executeAction(inCart, idProduct)}>
-                  <View style={{flexDirection: "row", marginLeft: 10}}>
-                    <Checkbox
-                      value={inCart}
-                      style={{height: 25, width: 25}}
-                    />
-                    <Text style={styles.labelCheckBox}>Adicionar ao carrinho</Text>
-                  </View>
-                </Pressable>
-                <Pressable onPress={onShare}>
-                  <IconFE style={{marginRight: 25, height: 30, width: 30}} name="share-2" size={27} />
-                </Pressable>
               </View>
             </View>
           </View>
-        </View>
-    </SafeAreaView>
-      </ScrollView>
+      </SafeAreaView>
+    </ScrollView>
   )
 }
